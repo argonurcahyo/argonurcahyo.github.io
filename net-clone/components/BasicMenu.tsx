24073137978996228a9ce46047ef370254f80f43
Argo Nurcahyo <argonurcahyo25@gmail.com>
2023-02-01 11:46:12 +0700

    day1

---
+++ net-clone/components/BasicMenu.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BasicMenu = () => {
+    return (
+        <div className='md:!hidden'>
+
+        </div>
+    )
+}
+
+export default BasicMenu
--- net-clone/pages/_app.tsx
+++ net-clone/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { RecoilRoot } from 'recoil'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      {/* Higher Order Component */}
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
 
 export default MyApp
